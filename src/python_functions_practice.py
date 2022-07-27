0fe38394b06c9c030366eef29c7897c6deae2083
Callum Donaldson <callumdonaldson1990@gmail.com>
2022-07-27 14:40:02 +0100

    Functions lab up to Jan date test

---
+++ src/python_functions_practice.py
@@ -0,0 +1,44 @@
+def return_10():
+    return 5 + 5
+
+def add(number1, number2):
+    return number1 + number2
+
+def subtract(number1, number2):
+    return number1 - number2
+
+def multiply(number1, number2):
+    return number1 * number2
+
+def divide(number1, number2):
+    return number1 / number2 
+
+def length_of_string(string_length):
+    return len(string_length)
+
+def join_string(string_1, string2):
+    return string_1 + string2
+
+def add_string_as_number(number1, number2):
+    converted_number_1 = int(number1)
+    converted_number_2 = int(number2)
+    return converted_number_1 + converted_number_2
+
+months = {
+    "1" : "January",
+    "2" : "February",
+    "3" : "March",
+    "4" : "April",
+    "5" : "May",
+    "6" : "June",
+    "7" : "July",
+    "8" : "August",
+    "9" : "September",
+    "10" : "October",
+    "11" : "November",
+    "12" : "December",
+ }
+
+def number_to_full_name_month_1(month_number):
+    return months["month_number"]
+
